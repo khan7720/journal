d4579c94da18007fd6a7a3a5ab67b5be73a10804
Kehan Zhao <52566203+khan7720@users.noreply.github.com>
2022-12-18 01:20:15 +0800

    Initial commit

---
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -0,0 +1,66 @@
+package com.example.plugins
+
+import com.example.dao.dao
+import io.ktor.server.application.*
+import io.ktor.server.freemarker.*
+import io.ktor.server.http.content.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.server.util.*
+
+fun Application.configureRouting() {
+    
+    routing {
+        static("/static") {
+            resources("files")
+        }
+        get("/") {
+            call.respondRedirect("articles")
+        }
+        route("articles") {
+            get {
+                // Show a list of articles
+                call.respond(FreeMarkerContent("index.ftl", mapOf("articles" to dao.allArticles())))            }
+            get("new") {
+                // Show a page with fields for creating a new article
+                call.respond(FreeMarkerContent("new.ftl", model = null))
+            }
+            post {
+                // Save an article
+                val formParameters = call.receiveParameters()
+                val title = formParameters.getOrFail("title")
+                val body = formParameters.getOrFail("body")
+                val article = dao.addNewArticle(title, body)
+                call.respondRedirect("/articles/${article?.id}")
+            }
+            get("{id}") {
+                // Show an article with a specific id
+                val id = call.parameters.getOrFail<Int>("id").toInt()
+                call.respond(FreeMarkerContent("show.ftl", mapOf("article" to dao.article(id))))
+            }
+            get("{id}/edit") {
+                // Show a page with fields for editing an article
+                val id = call.parameters.getOrFail<Int>("id").toInt()
+                call.respond(FreeMarkerContent("edit.ftl", mapOf("article" to dao.article(id))))
+            }
+            post("{id}") {
+                // Update or delete an article
+                val id = call.parameters.getOrFail<Int>("id").toInt()
+                val formParameters = call.receiveParameters()
+                when (formParameters.getOrFail("_action")) {
+                    "update" -> {
+                        val title = formParameters.getOrFail("title")
+                        val body = formParameters.getOrFail("body")
+                        dao.editArticle(id, title, body)
+                        call.respondRedirect("/articles/$id")
+                    }
+                    "delete" -> {
+                        dao.deleteArticle(id)
+                        call.respondRedirect("/articles")
+                    }
+                }
+            }
+        }
+    }
+}
